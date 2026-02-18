cf8213d62e2c64609411ed9d122db02037d875ce
bizzyjack <53256768+bizzyjack@users.noreply.github.com>
2026-02-18 14:56:13 +0100

    Delete 00_load_packages.R

--- 00_load_packages.R
+++
@@ -1,8 +0,0 @@
-pkgs <- c(
-  "tidyverse", "tidymodels", "naniar", "survey", "srvyr", "visdat", "ranger", "vip",
-  "janitor", "forcats", "gtsummary", "gt", "broom", "VIM", "effectsize", "smd",
-  "yardstick", "DALEX", "patchwork", "fairmodels", "ggrepel", "scales", "corrplot",
-  "pmsampsize", "haven"
-)
-
-invisible(lapply(pkgs, library, character.only = TRUE))
